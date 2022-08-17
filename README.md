1b76bd054e4e80a82c054cad6a7b337d82b2aba9
PHAM NGUYEN MY DIEM <diempham@genesolutions.vn>
2022-08-17 18:21:46 +0700

    update predict.py

--- README.md
+++ README.md
@@ -1,36 +1,33 @@
 # epiTCR
-A highly sensitive TCR-peptide binding predictor for bulk sequencing data 
+A highly sensitive predictor for TCR-peptide binding 
 
 
 ## Requirements
 ```text
-python >= 3.6.8
+python >= 3.x
 numpy 1.22.4
 scikit-learn 1.1.2
 ```
 
 ## Model Training
-The main module for training is `epiTCR.py`.
-For training, run:
-```commandline
+The main module for training is `epiTCR.py`. You can train the epiTCR without mhc models running
 
+```commandline
+python3 epiTCR.py --trainfile data/split-data/without-mhc/train/train.csv --testfile data/split-data/without-mhc/test/test01.csv 
 ```
-where:
+This will print the predictions on the standard output or on a file (that can be specified with the option --outfile).
+
+Both training and test set should be a comma-separated CSV files. The files should have the following columns (with headers): CDR3b, epitope, MHC (full-length), binder (the binder coulmn is not required in the test file). See data/split-data/without-mhc/train/train.csv and data/split-data/without-mhc/test/test01.csv as an example.
 
 
 ## Binding Prediction
 
-Anyway you can also predict using the `epiTCR.py` module.
+You can predict using the `predict.py` module.
 It is quite similar to training, run:
 ```commandline
-
-```
-where:
-
-## Example
-The example input file is under test/input/.
-```commandline
-
+python3 predict.py --testfile data/split-data/without-mhc/test/test01.csv 
 ```
 
 ## Output file example
+epiTCR without mhc outputs a table with 4 columns: CDR3b sequences, epitopes sequences, MHC full length, and predict for each pair of TCR/epitope.
+
---
+++ predict.py
@@ -0,0 +1,92 @@
+import numpy as np
+import os, sys
+import pickle
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sklearn.metrics as metrics
+
+from sklearn.model_selection import train_test_split, cross_validate, GridSearchCV
+from sklearn.metrics import classification_report,roc_curve,auc, f1_score, plot_roc_curve
+from sklearn.metrics import confusion_matrix,accuracy_score,precision_score,recall_score,roc_auc_score
+from sklearn.ensemble import RandomForestClassifier
+from imblearn.under_sampling import RandomUnderSampler
+import warnings
+
+from tqdm import tqdm
+from IPython.display import display
+import src.modules.processor as Processor
+import src.modules.utils as Utils
+# import src.modules.model as Model
+import src.modules.plot as Plot
+
+from argparse import ArgumentParser
+
+class epitcrModel:
+    def __init__(self, pmodel, pX, py):
+        self.model = pmodel
+        self.model.fit(pX, py)
+    
+    def predict(self, pnew_data):
+        yhat_class = self.model.predict(pnew_data)
+        return yhat_class 
+
+    def predict_proba(self, pnew_data):
+        yhat_class = self.model.predict_proba(pnew_data)
+        return yhat_class 
+
+#Args parse
+parser = ArgumentParser(description="Specifying Input Parameters")
+parser.add_argument("-te", "--testfile", help="Specify the full path of the file with TCR sequences")
+parser.add_argument("-mf", "--model_file", default="nm", help="Specify the model file(s) to use (nm, m). Default: nm")
+parser.add_argument("-o", "--outfile", default=sys.stdout, help="Specify output file")
+args = parser.parse_args()
+
+model_file = args.model_file
+
+if model_file not in ["nm","m"]:
+    print("Invalid chain. You can select nm (model without mhc), m (model with mhc)")
+
+test = pd.read_csv(args.testfile)
+
+clf_sm = RandomUnderSampler(random_state=42)
+
+if(model_file=='nm'):
+    X_test = test.iloc[:, :2]
+    y_test = test.iloc[:, 2:]
+
+    pX_test = Processor.data_representation(X_test)
+    py_test = y_test.copy()
+
+    model_rf = pickle.load(open('./src/models/rdforest-model.pickle', 'rb'))
+    print('Evaluating..')
+    
+    y_rf_test_proba = model_rf.predict_proba(pX_test)
+    df_test = pd.DataFrame(data = y_rf_test_proba, columns = ["tmp", "predict_proba"])
+
+    df_test = df_test.iloc[:, 1:]
+
+    df_prob_test = pd.concat([test, df_test], axis=1)
+    df_prob_test.to_csv('output_prediction.csv', index=False)
+    # df_prob_test.to_csv(args.outfile, index=False)
+    print('Done!')
+
+
+elif model_file=="m":
+    X_test_mhc = test.iloc[:, :3]
+    y_test_mhc = test.iloc[:, 3:]
+
+    pX_test_mhc = Processor.data_representation_mhc(X_test_mhc)
+    py_test_mhc = y_test_mhc.copy()
+
+    model_rf_mhc = pickle.load(open('./src/models/rdforest-model-mhc.pickle', 'rb'))
+    print('Evaluating..')
+    y_rf_test_proba_mhc = model_rf_mhc.predict_proba(pX_test_mhc)
+    df_test_mhc = pd.DataFrame(data = y_rf_test_proba_mhc, columns = ["tmp", "predict_proba"])
+
+    df_test_mhc = df_test_mhc.iloc[:, 1:]
+
+    df_prob_test_mhc = pd.concat([test, df_test_mhc], axis=1)
+    df_prob_test_mhc.to_csv('output_prediction.csv', index=False)
+    # df_prob_test_mhc.to_csv(args.outfile, index=False)
+    print('Done!')
