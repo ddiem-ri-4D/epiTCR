9ef7b9a274420d7bc39d3a8d16ec095dddc9cbee
Phạm Nguyễn Mỹ Diễm <50079932+ddiem-ri-4D@users.noreply.github.com>
2024-08-06 00:59:08 +0700

    Update convertPeusoSeq.py

--- src/hlaCovertPeudoSeq/convertPeusoSeq.py
+++ src/hlaCovertPeudoSeq/convertPeusoSeq.py
@@ -1,18 +1,28 @@
 import numpy as np
 import pandas as pd
-from utils import *
+from utils import hla_old, hla_new
 
-DATA_CV = pd.read_csv('../data/finalData/finalWithHLA.csv')
+# Read the final dataset containing HLA data
+data_cv = pd.read_csv('../data/finalData/finalWithHLA.csv')
 
-DATA_CV['HLA'] = DATA_CV['HLA'].replace(hla_old, hla_new)
+# Replace old HLA names with new HLA names
+data_cv['HLA'] = data_cv['HLA'].replace(hla_old, hla_new)
 
+# Read the HLA pseudo sequence data
 data_hla = pd.read_csv("../data/hlaCovertPeudoSeq/HLAWithPseudoSeq.csv")
-data_mhc = pd.DataFrame(hla_new, columns = ["HLA name"])
+
+# Create a DataFrame with new HLA names
+data_mhc = pd.DataFrame(hla_new, columns=["HLA name"])
+
+# Merge the HLA data with pseudo sequences based on HLA name
 result = pd.merge(data_mhc, data_hla, on=["HLA name"])
 
-lst_HLA = np.array(result["HLA name"])
+# Extract lists of HLA names and their corresponding pseudo sequences
+lst_hla = np.array(result["HLA name"])
 lst_imgt = np.array(result["HLA PSEUDO SEQ"])
 
-DATA_CV['MHC'] = DATA_CV['HLA'].replace(lst_HLA, lst_imgt)
+# Replace HLA names in the dataset with their pseudo sequences
+data_cv['MHC'] = data_cv['HLA'].replace(lst_hla, lst_imgt)
 
-DATA_CV.to_csv('../data/finalData/finalWithHLAConverted.csv', index=False)
+# Save the updated dataset to a new CSV file
+data_cv.to_csv('../data/finalData/finalWithHLAConverted.csv', index=False)
